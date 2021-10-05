61ee25dd4a3dfac597bf7111b7f838920def700a
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2021-10-05 10:03:51 -0500

    se implemento la navegacion de la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { SharedModule } from './shared/shared.module';
 
+import { AppRouterModule } from './app-router.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule],
+  imports: [BrowserModule, AppRouterModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/sales/sales.module.ts
+++ src/app/sales/sales.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NumbersComponent } from './pages/numbers/numbers.component';
+import { NonCommonComponent } from './pages/non-common/non-common.component';
+import { BasicsComponent } from './pages/basics/basics.component';
+import { OrderingComponent } from './pages/ordering/ordering.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    NumbersComponent,
+    NonCommonComponent,
+    BasicsComponent,
+    OrderingComponent,
+  ],
+  imports: [CommonModule],
+  exports: [
+    BasicsComponent,
+    NonCommonComponent,
+    NumbersComponent,
+    OrderingComponent,
+  ],
 })
-export class SalesModule { }
+export class SalesModule {}
