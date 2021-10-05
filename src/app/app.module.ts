9c39d86da5bbc39e95c79c4f3f8c8fc268b7f629
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2021-10-05 09:29:59 -0500

    se importo el PrimeNGModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
-import { ButtonModule } from 'primeng/button';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ButtonModule],
+  imports: [BrowserModule, PrimeNgModule],
   providers: [],
   bootstrap: [AppComponent],
 })
