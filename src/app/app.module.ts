aac4a4f69edd8cf49100652e51c2cc7d6761899b
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2021-10-05 09:50:45 -0500

    se implemento el menu bar y se elimino la importacion inncesaria de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
-import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PrimeNgModule],
+  imports: [BrowserModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
   declarations: [],
   imports: [],
-  exports: [ButtonModule, CardModule],
+  exports: [ButtonModule, CardModule, MenubarModule],
 })
 export class PrimeNgModule {}
