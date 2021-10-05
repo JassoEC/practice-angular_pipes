caab468dc1c8086975149096b53bc64afd984374
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2021-10-05 10:03:02 -0500

    se crearon los componentes de rutas de la aplicacion

---
+++ src/app/sales/pages/non-common/non-common.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-non-common',
+  templateUrl: './non-common.component.html',
+  styles: [
+  ]
+})
+export class NonCommonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/sales/pages/ordering/ordering.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ordering',
+  templateUrl: './ordering.component.html',
+  styles: [
+  ]
+})
+export class OrderingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
