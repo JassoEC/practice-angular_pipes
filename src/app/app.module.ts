c73a60cc42540026117198ed23d300b7c742d4fd
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2021-10-06 10:22:47 -0500

    se agrego el ejemplo del registro global del locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { SharedModule } from './shared/shared.module';
@@ -7,10 +7,16 @@ import { AppRouterModule } from './app-router.module';
 import { AppComponent } from './app.component';
 import { SalesModule } from './sales/sales.module';
 
+// Cambiar locale de forma global
+
+import localMX from '@angular/common/locales/es-MX';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localMX);
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRouterModule, SharedModule, SalesModule],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'es-MX' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
