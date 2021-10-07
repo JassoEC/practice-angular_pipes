c39656f1f902767eead011861bb6abd0ee088d9e
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2021-10-07 10:12:46 -0500

    se agrego el ejemplo del uso del pipe i18nSelect

--- src/app/sales/pages/non-common/non-common.component.ts
+++ src/app/sales/pages/non-common/non-common.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-non-common',
   templateUrl: './non-common.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class NonCommonComponent implements OnInit {
+export class NonCommonComponent {
+  name: string = 'Emanuel';
+  gender: string = 'female';
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  invitation = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 }
